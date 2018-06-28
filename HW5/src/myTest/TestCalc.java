7811facb672a089dd8de14a5571f88c56d58ca48
AlexLundinEducational <31811167+AlexLundinEducational@users.noreply.github.com>
2018-06-28 11:45:52 -0500

    HW5 phase 2

--- HW5/src/myTest/TestCalc.java
+++ HW5/src/myTest/TestCalc.java
@@ -1,38 +1,58 @@
 package myTest;
 import myClass.DaysDurationCalculator;
 
-import org.junit.Before;
+import java.util.Arrays;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import static org.junit.Assert.assertTrue;
 
-import static org.junit.Assert.*;
+//Alex Lundin
+//06-28-2018
+//SE-4367-Testing
 
+//HW5 10 tests
+//this design pattern uses a parameterized data driven testing approach
+//the tests focus on normal behavior of the cal method
+
+@RunWith(Parameterized.class)
 public class TestCalc {
-	private static DaysDurationCalculator tempCalc;
-	private int month1, day1, month2, day2, year;
-	@Before 
-	public void Setup(){
-
-	}
 	
-	@Test
-	public void TestCalc1(){
-		
-		
-		int T;
+	@Parameters
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(new Object[][] { {1, 1, 2, 1, 2018, 31},
+					{1, 1, 2, 1, 2018, 32} });
+    }
+    
+    // private fields, used only in the test Class
+    private int month1;
+    private int day1;
+    private int month2;
+    private int day2;
+    private int year;
+    private int expectedDaysBetween;
+    private int actualDaysBetween;
 
-		month1 = 1;
-		day1 = 1;
-		month2 = 2;
-		day2 = 2;
-		year = 2018;
-		// preconditions : day1 and day2 must be in same year
-		//               1 <= month1, month2 <= 12
-		//               1 <= day1, day2 <= 31
-		//               month1 <= month2
-		//               The range for year: 1 ... 10000
-		T = tempCalc.cal (month1, day1, month2, day2, year);
-		System.out.println ("Result is: " + T);
-		
-	}
-	
+    public TestCalc(int argMonth1, int argDay1, int argMonth2, int argDay2, int argYear, int argExpectedDaysBetween) {
+        // store values from parameter array
+    	this.month1 = argMonth1;
+        this.day1 = argDay1;
+        this.month2 = argMonth2;
+        this.day2 = argDay2;
+        this.year = argYear;
+        this.expectedDaysBetween = argExpectedDaysBetween;
+        
+    }
+    
+    @Test
+    public void AssertFixture(){
+        // actually use the cal method
+    	this.actualDaysBetween = DaysDurationCalculator.cal (month1, day1, month2, day2, year);
+    	System.out.println ("Result is: " + this.actualDaysBetween);    	
+    	  	
+    	// test the actual and the expected results
+    	assertTrue("Test failed:", this.actualDaysBetween == this.expectedDaysBetween);
+    }
+    
 }
