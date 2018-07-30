4028eac22d005b7db53f02137d17b5d497429757
AlexLundinEducational <31811167+AlexLundinEducational@users.noreply.github.com>
2018-07-29 22:03:43 -0500

    HW8 started

---
+++ HW8/src/myClasses/Period.java
@@ -0,0 +1,6 @@
+package myClasses;
+// Used with the Thermostat.java example, Chapter 8
+public enum Period
+{
+   MORNING, DAY, EVENING, NIGHT
+}
---
+++ HW8/src/myTests/ThermostatTest_CACC.java
@@ -0,0 +1,83 @@
+package myTests;
+
+import myClasses.Thermostat;
+import myClasses.ProgrammedSettings;
+import myClasses.DayType;
+import myClasses.Period;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ThermostatTest_CACC {
+	public Thermostat thermo;
+	public ProgrammedSettings settings;
+	
+	@Before
+	public void Setup(){
+		thermo = new Thermostat(); // Needed object
+		settings = new ProgrammedSettings(); // Needed object
+		settings.setSetting (Period.MORNING, DayType.WEEKDAY, 69); // dTemp
+		thermo.setPeriod (Period.MORNING); // dTemp		
+	}
+
+	@Test
+	public void TestTTFT(){
+		thermo.setCurrentTemp (63);
+		thermo.setThresholdDiff (5);
+		thermo.setOverride (true);
+		thermo.setOverTemp (67); // c is false
+		thermo.setMinLag (10);
+		thermo.setTimeSinceLastRun (12);		
+	}
+	
+	@Test
+	public void TestFTFT(){
+		thermo.setCurrentTemp (66); // a is false
+		thermo.setThresholdDiff (5);
+		thermo.setOverride (true);
+		thermo.setOverTemp (67); // c is false
+		thermo.setMinLag (10);
+		thermo.setTimeSinceLastRun (12);		
+	}
+
+	@Test
+	public void TestFTTT(){
+		thermo.setCurrentTemp (66); // a is false
+		thermo.setThresholdDiff (5);
+		thermo.setOverride (true);
+		thermo.setOverTemp (72); // to make c true
+		thermo.setMinLag (10);
+		thermo.setTimeSinceLastRun (12);		
+	}
+
+	// left off here, editing actual tests
+	@Test
+	public void TestFFTT(){
+		thermo.setCurrentTemp (66); // a is false
+		thermo.setThresholdDiff (5);
+		thermo.setOverride (false); // b is false
+		thermo.setOverTemp (72);
+		thermo.setMinLag (10);
+		thermo.setTimeSinceLastRun (12);
+	}
+	
+	@Test
+	public void TestTTTT(){
+		thermo.setCurrentTemp (63);
+		thermo.setThresholdDiff (5);
+		thermo.setOverride (true);
+		thermo.setOverTemp (72);
+		thermo.setMinLag (10);
+		thermo.setTimeSinceLastRun (12);		
+	}
+	
+	@Test
+	public void TestTTTF(){
+		thermo.setCurrentTemp (63);
+		thermo.setThresholdDiff (5);
+		thermo.setOverride (true);
+		thermo.setOverTemp (72);
+		thermo.setMinLag (10);
+		thermo.setTimeSinceLastRun (8); // d is false	
+	}	
+}
